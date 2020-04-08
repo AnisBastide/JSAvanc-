e7c6cbc891561434969f998727788378af13bf8d
Buronfosse <buronfosseetienne@gmail.com>
2020-04-08 16:30:27 +0200

    tableau fait

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -30,7 +30,37 @@ class Home extends React.Component {
         return tab;
     }
 
+    generateIndiceHaut(column){
+        var tab = [];
+        var tab2 = [];
+        for (var i = 0; i < 1; i++) {
+            for (var j = 0; j < column; j++) {
+                tab2.push([]);
+            }
+            tab.push(tab2);
+            tab2 = [];
+        }
+        console.log(tab);
+        return tab;
+    }
+    generateIndiceGauche(line){
+        var tab = [];
+        var tab2 = [];
+        for (var i = 0; i < line; i++) {
+            for (var j = 0; j < 1; j++) {
+                tab2.push([]);
+
+            }
+            tab.push(tab2);
+            tab2 = [];
+        }
+        console.log(tab);
+        return tab;
+    }
+
+
     render() {
+        var tabIndices = ["1", "2", "3", "4", "5"];
         const {board} = this.props;
         //const { profil } = this.props;
         /*return (
@@ -48,7 +78,27 @@ class Home extends React.Component {
                     <input id="line" type="text" name="line" defaultValue={5}/> <br/>
                     <input type="submit"/>
                 </form>
-                <div>
+                <div id="decal-right">
+                    <table class=".indice">
+                    <tbody>{this.generateIndiceHaut(board.line).map((tab, i) => (
+                        <tr key={i}>
+                            {tab.map((tab,i)=>(
+                                <td key={i} className="indice"><div class="column"><div>{tabIndices[0]}</div> <div>{tabIndices[3]}</div> <div>{tabIndices[2]}</div></div></td>
+                            ))}
+                        </tr>
+                    ))}</tbody>
+                    </table>
+                </div>
+                <div id="align">
+                    <table>
+                        <tbody>{this.generateIndiceGauche(board.column).map((tab, i) => (
+                            <tr key={i}>
+                                {tab.map((tab,i)=>(
+                                    <td key={i} className="indice"><div class="line"><div>1</div><div>2</div><div>3</div></div></td>
+                                ))}
+                            </tr>
+                        ))}</tbody>
+                    </table>
                 <table>
                     <tbody>{this.generateGrid(board.line, board.column).map((tab, i) => (
                         <tr key={i}>
